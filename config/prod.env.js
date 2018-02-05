05df486e269bcdfe09030856615349de950e5d40
todoroki <todoroki.godai@gmail.com>
2018-02-05 14:12:42 +0900

    fix production settings

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-	BASE_API: '"https://api-prod"'
+	BASE_API: '"http://198.13.37.84:18080/api/v1.0"',
+	DASHBOARD_CHART_DEFAULT_TERM: 30,
 }
