8ed40410064b5ed32eba7550d9c4ca74f40220b3
todoroki <todoroki.godai@gmail.com>
2018-02-11 14:01:44 +0900

    change url for https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-	BASE_API: '"http://198.13.37.84:18080/api/v1.0"',
+  BASE_API: '"https://coin-family.peace-coin.org/api/v1.0"',
 	DASHBOARD_CHART_DEFAULT_TERM: 30,
 }
